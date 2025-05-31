a3e71dab8178a991cf85535d4c81840c55c80470
Shlomi Fish <shlomif@shlomifish.org>
2025-05-31 06:44:45 +0300

    [WiP] convert SSG to template toolkit 2

---
+++ bin/tt-render.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use 5.014;
+
+use lib './lib';
+
+use Getopt::Long                   qw/ GetOptions /;
+use Parallel::Map::Segmented       ();
+use Path::Tiny                     qw/ path /;
+use Shlomif::LMSolveSite::TTRender ();
+
+my $printable;
+my $stdout;
+my @filenames;
+
+GetOptions(
+    'fn=s'       => \@filenames,
+    'printable!' => \$printable,
+    'stdout!'    => \$stdout,
+) or die $!;
+
+my $obj = Shlomif::LMSolveSite::TTRender->new(
+    { printable => $printable, stdout => $stdout, } );
+
+if ( !@filenames )
+{
+    @filenames = path("lib/make/tt2.txt")->lines_raw( { chomp => 1 } );
+}
+
+Parallel::Map::Segmented->new->run(
+    {
+        #         disable_fork => 1,
+        items         => \@filenames,
+        nproc         => 1,
+        batch_size    => 100,
+        process_batch => sub {
+            my ($aref) = @_;
+            foreach my $fn (@$aref)
+            {
+                $obj->proc($fn);
+            }
+            return;
+        },
+    }
+);
